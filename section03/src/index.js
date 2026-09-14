57c8c5ee15ac14d61dc9ecb76f77984b4bd9ee06
bize <bizecommission@gmail.com>
2026-09-14 23:38:32 +0900

    section 3 node.js 파트

---
+++ section03/src/index.js
@@ -0,0 +1,13 @@
+import {add, sub} from './math.js';
+import randomColor from 'randomcolor';
+
+const color = randomColor()
+console.log(color)
+
+// console.log(moduleData.add(1,2));
+// console.log(moduleData.sub(1,2));
+
+// console.log(add(1,2));
+// console.log(sub(1,2))
+
+// console.log(moduleData);
