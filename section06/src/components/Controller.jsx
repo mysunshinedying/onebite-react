dbf9bd9a3c095973b21f3590c3dbd2bd019364a8
bize <bizecommission@gmail.com>
2026-09-20 15:39:08 +0900

    section 07 카운터앱 구현

---
+++ section06/src/components/Controller.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Controller = ({onClickButton}) => {
+    return (
+        <div>
+            <button
+                type="button"
+                onClick={() => {
+                    onClickButton(-1);
+                }}>-1
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    onClickButton(-10);
+                }}
+            >-10
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    onClickButton(-100);
+                }}
+            >-100
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    onClickButton(100);
+                }}
+            >+100
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    onClickButton(10);
+                }}
+            >+10
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    onClickButton(1);
+                }}
+            >+1
+            </button>
+        </div>
+    );
+};
+
+export default Controller;
