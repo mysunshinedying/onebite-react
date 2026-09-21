196356be85d64a34440d11a80ab80539d0ee867f
bize <bizecommission@gmail.com>
2026-09-21 17:33:52 +0900

    Section 07 React useEffect, 컴포넌트 생명주기

---
+++ section07/src/components/Even.jsx
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+
+const Even = () => {
+    useEffect(() => {
+        //클린 업, 정리함수
+
+        return () => {
+            console.log('unmount');
+        }
+    }, []);
+    return (
+        <div>
+            짝수입니다.
+        </div>
+    );
+};
+
+export default Even;
